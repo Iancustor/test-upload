de558360ddbabfcbcf63a0ac542dbea109a71eac
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-05-16 17:11:37 +0300

    updated with upload thing

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { UploadButton, UploadDropzone } from "@/utils/uploadthing";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 import { user } from "@/types/types";
+
 function page() {
   const {
     register,
@@ -14,7 +15,10 @@ function page() {
     formState: { errors },
   } = useForm<user>();
   const [imageUrl, setImageUrl] = useState("");
-  function submitData(data: user) {
+
+  // console.log(imageUrl);
+  async function submitData(data: user) {
+    data.image = imageUrl;
     console.log(data);
   }
   return (
@@ -51,9 +55,10 @@ function page() {
                   Your Name
                 </label>
                 <input
-                  {...(register("name"), { required: true })}
+                  {...register("name", { required: true })}
                   type="text"
                   id="name"
+                  name="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-violet-600 focus:border-violet-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="John Doe"
                 />
@@ -69,9 +74,10 @@ function page() {
                   Your email
                 </label>
                 <input
-                  {...(register("email"), { required: true })}
+                  {...register("email", { required: true })}
                   type="email"
                   id="email"
+                  name="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-violet-600 focus:border-violet-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                 />
@@ -106,6 +112,7 @@ function page() {
                 </div>
               ) : null}
               <button
+                onSubmit={handleSubmit(submitData)}
                 type="submit"
                 className="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
               >
